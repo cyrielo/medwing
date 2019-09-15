2f328e42a301fda03f7b24734fdde3e368086d65
Cyril Ologho <cyril.paul@andela.com>
2019-09-15 10:10:00 +0100

    add layout for marker controls and action

--- src/App.js
+++ src/App.js
@@ -10,6 +10,38 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
         </div>
       </header>
+      <div className="container">
+        <div className="col map">
+
+        </div>
+        <div className="col marker-controls">
+          <div className="add-marker">
+            <button className="btn btn-sm">Add Marker</button>
+          </div>
+          <div className="markers">
+            <ul className="marker-list">
+              <li className="marker-item">
+                <h2>Checkpoint Charlie, Berlin</h2>
+                <div className="location-name">Checkpoint Charlie, Berlin</div>
+                <div className="lat">Latitude: <span>52.5074434</span></div>
+                <div className="lng">Longitude: <span>13.3903913</span></div>
+                <div className="marker-actions">
+                  <button>Edit</button> <button>Delete</button>
+                </div>
+              </li>
+              <li className="marker-item">
+                <h2>Leipzig</h2>
+                <div className="location-name">Leipzig</div>
+                <div className="lat">Latitude: <span>52.5074434</span></div>
+                <div className="lng">Longitude: <span>13.3903913</span></div>
+                <div className="marker-actions">
+                  <button>Edit</button> <button>Delete</button>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
