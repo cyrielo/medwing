dffa9054950e5140d95af7586128f3cd58c6c64f
Cyril Ologho <cyril.paul@andela.com>
2019-09-16 14:32:38 +0100

    improve style

--- src/App.js
+++ src/App.js
@@ -12,30 +12,45 @@ function App() {
       </header>
       <div className="container">
         <div className="col map">
-
+          <iframe  id="map-embed" src="https://maps.google.com/maps?q=leipzig&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
         </div>
         <div className="col marker-controls">
           <div className="add-marker">
-            <button className="btn btn-sm">Add Marker</button>
+            <button className="btn btn-primary">Add Marker</button>
           </div>
           <div className="markers">
             <ul className="marker-list">
               <li className="marker-item">
-                <h2>Checkpoint Charlie, Berlin</h2>
+                <h2 className="location-title">Checkpoint Charlie, Berlin</h2>
                 <div className="location-name">Checkpoint Charlie, Berlin</div>
-                <div className="lat">Latitude: <span>52.5074434</span></div>
-                <div className="lng">Longitude: <span>13.3903913</span></div>
+                <div className="coordinates">
+                  <div className="lat">Latitude: <span>52.5074434</span></div>
+                  <div className="lng">Longitude: <span>13.3903913</span></div>
+                </div>
                 <div className="marker-actions">
-                  <button>Edit</button> <button>Delete</button>
+                  <button className="btn">Edit</button> <button className="btn">Delete</button>
                 </div>
               </li>
               <li className="marker-item">
-                <h2>Leipzig</h2>
+                <h2 className="location-title">Leipzig</h2>
                 <div className="location-name">Leipzig</div>
-                <div className="lat">Latitude: <span>52.5074434</span></div>
-                <div className="lng">Longitude: <span>13.3903913</span></div>
+                <div className="coordinates">
+                  <div className="lat">Latitude: <span>52.5074434</span></div>
+                  <div className="lng">Longitude: <span>13.3903913</span></div>
+                </div>
                 <div className="marker-actions">
-                  <button>Edit</button> <button>Delete</button>
+                  <button className="btn">Edit</button> <button className="btn">Delete</button>
+                </div>
+              </li>
+              <li className="marker-item">
+                <h2 className="location-title">Leipzig</h2>
+                <div className="location-name">Leipzig</div>
+                <div className="coordinates">
+                  <div className="lat">Latitude: <span>52.5074434</span></div>
+                  <div className="lng">Longitude: <span>13.3903913</span></div>
+                </div>
+                <div className="marker-actions">
+                  <button className="btn">Edit</button> <button className="btn">Delete</button>
                 </div>
               </li>
             </ul>
